ebccd1c8779c5e03fdaf58f967a5edc9d1ed02a5
K0unna00 <78pf4jz@code.edu.az>
2022-03-15 01:45:18 +0400

    Tasks7

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1th
+
+            /*
+           
+            int[] array = { 12, 334, 32, 45 };
+            int count = 0;
+            foreach (int i in array)
+            {
+                int j = i;
+                int sum = 0;
+                while (j != 0)
+                {
+                    sum += j % 10;
+                    j /= 10;
+                }
+                if (sum < 10)
+                    count++;
+
+            }
+            Console.WriteLine(count);
+            
+            */
+
+
+
+            //2nd
+
+            /*
+             * 
+            int count = 0;
+            for(int i = 1; i < 8000; i++)
+            {
+                if (i % 2 == 0)
+                    count++;
+            }
+            Console.WriteLine(count);
+
+            /*
+
+
+            //3rd task
+
+            /*
+
+            int num = 31 , i=0;
+            for(i=0;i<num;i++){
+                if(i*i>num){
+                    Console.WriteLine(i-1);
+                    break;
+                }
+            }
+            i=0;
+            while (i < num)
+            {
+                if (i * i > num)
+                {
+                    Console.WriteLine(i - 1);
+                    break;
+                }
+                i++;
+            }
+
+
+            */
+
+        }   
+    }
+}
